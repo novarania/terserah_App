c7fa7a2c6672837254847bdd16b25198a21bcf33
novaraniafifah <novaraniafifah@gmail.com>
2015-06-22 19:01:15 +0700

    Menambah App Bar pada tiap layout

--- app/src/main/java/com/aceculture/terserahapp/MainActivity.java
+++ app/src/main/java/com/aceculture/terserahapp/MainActivity.java
@@ -2,26 +2,35 @@ package com.aceculture.terserahapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toolbar;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        //setSupportActionBar(toolbar);
+
 
         ImageButton jadwal_Btn = (ImageButton) findViewById(R.id.jadwal_btn);
         jadwal_Btn.setOnClickListener((View.OnClickListener) this);
         ImageButton buka_Btn = (ImageButton) findViewById(R.id.buka_btn);
         buka_Btn.setOnClickListener((View.OnClickListener) this);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
