fea8222d21e8e2eba8f2d9fe128da697f04f7688
novaraniafifah <novaraniafifah@gmail.com>
2015-06-23 10:01:19 +0700

    Edit Layout jadwal_imsak_dan_sholat & Tambah Activity detail_tempat_makan

---
+++ app/src/main/java/com/aceculture/terserahapp/detail_tempat_makan.java
@@ -0,0 +1,54 @@
+package com.aceculture.terserahapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class detail_tempat_makan extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tempat_makan);
+        setupToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail_tempat_makan, menu);
+        return true;
+    }
+    private void setupToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        if(toolbar != null)
+            setSupportActionBar(toolbar);
+
+        // Show menu icon
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (item.getItemId() == android.R.id.home) {
+            Intent j = new Intent(detail_tempat_makan.this, tempat_berbuka.class);
+            startActivity(j);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
