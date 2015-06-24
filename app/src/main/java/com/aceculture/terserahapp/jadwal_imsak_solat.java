585fbff4d0c8bcdb9fcc72901fdfc19a0e6a13e6
radityacandra <radityacandra@gmail.com>
2015-06-24 13:47:58 +0700

    add algoritma cari tahu sisa waktu sholat berikutnya. masih ada bug, kalau habis isya (sampe kurang dari jam 24) harusnya error pembacaan sisa waktu

--- app/src/main/java/com/aceculture/terserahapp/jadwal_imsak_solat.java
+++ app/src/main/java/com/aceculture/terserahapp/jadwal_imsak_solat.java
@@ -21,7 +21,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import static android.text.TextUtils.substring;
 
 public class jadwal_imsak_solat extends AppCompatActivity {
     Toolbar toolbar;
@@ -47,19 +51,6 @@ public class jadwal_imsak_solat extends AppCompatActivity {
         TextView waktu_isya = (TextView) findViewById(R.id.waktuIsya);
 
         DigitalClock dc = (DigitalClock) findViewById(R.id.digitalClock1);
-
-        //text_menit.setText("text");
-        new CountDownTimer(30000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-
-                text_menit.setText(String.valueOf(millisUntilFinished / 1000));
-            }
-
-            public void onFinish() {
-                text_menit.setText("done!");
-            }
-        }.start();
 
         //buat nyari tahu sekarang tanggal berapa
         Calendar cal = Calendar.getInstance();
@@ -153,10 +144,128 @@ public class jadwal_imsak_solat extends AppCompatActivity {
                 waktu_asar.setText(jsonObj.getString("Asr"));
                 waktu_maghrib.setText(jsonObj.getString("Maghrib"));
                 waktu_isya.setText(jsonObj.getString("Isha"));
+
+                waktuImsyak = jsonObj.getString("Imsaak");
+                waktuSubuh = jsonObj.getString("Fajr");
+                waktuDhuhur = jsonObj.getString("Dhuhr");
+                waktuAshar = jsonObj.getString("Asr");
+                waktuMaghrib = jsonObj.getString("Maghrib");
+                waktuIsya = jsonObj.getString("Isha");
+
+                PenentuanWaktuSolat();
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static String waktuImsyak;
+    public static String waktuSubuh;
+    public static String waktuDhuhur;
+    public static String waktuAshar;
+    public static String waktuMaghrib;
+    public static String waktuIsya;
+
+    public void PenentuanWaktuSolat(){
+        //deklarasi element view
+        TextView text_jam = (TextView) findViewById(R.id.textjam);
+        TextView text_menit = (TextView) findViewById(R.id.textmenit);
+        TextView sholat = (TextView) findViewById(R.id.Sholat);
+        //deklarasi variabel lokal method
+        int sisaImsyak, sisaSubuh, sisaDhuhur, sisaAshar, sisaMaghrib, sisaIsya;
+        int sisaJam, sisaMenit;
+
+        sisaImsyak = SisaWaktuSolat(waktuImsyak);
+        sisaSubuh = SisaWaktuSolat(waktuSubuh);
+        sisaDhuhur = SisaWaktuSolat(waktuDhuhur);
+        sisaAshar = SisaWaktuSolat(waktuAshar);
+        sisaMaghrib = SisaWaktuSolat(waktuMaghrib);
+        sisaIsya = SisaWaktuSolat(waktuIsya);
+
+        //cari waktu terdekat
+        if (sisaImsyak<sisaSubuh && sisaImsyak>0 && sisaSubuh>0){
+            sisaJam = sisaImsyak/60;
+            sisaMenit = sisaImsyak%60;
+            text_jam.setText(String.valueOf(sisaJam));
+            text_menit.setText(String.valueOf(sisaMenit));
+            sholat.setText("Imsyak");
+        }
+        else if (sisaSubuh<sisaDhuhur && sisaSubuh>0 && sisaDhuhur>0){
+            sisaJam = sisaSubuh/60;
+            sisaMenit = sisaSubuh%60;
+            text_jam.setText(String.valueOf(sisaJam));
+            text_menit.setText(String.valueOf(sisaMenit));
+            sholat.setText("Subuh");
+        }
+        else if (sisaDhuhur<sisaAshar && sisaDhuhur>0 && sisaAshar>0){
+            sisaJam = sisaDhuhur/60;
+            sisaMenit = sisaDhuhur%60;
+            text_jam.setText(String.valueOf(sisaJam));
+            text_menit.setText(String.valueOf(sisaMenit));
+            sholat.setText("Dhuhur");
+        }
+        else if (sisaAshar<sisaMaghrib && sisaAshar>0 && sisaMaghrib>0){
+            sisaJam = sisaAshar/60;
+            sisaMenit = sisaAshar%60;
+            text_jam.setText(String.valueOf(sisaJam));
+            text_menit.setText(String.valueOf(sisaMenit));
+            sholat.setText("Ashar");
+        }
+        else if (sisaMaghrib<sisaIsya && sisaMaghrib>0 && sisaIsya>0){
+            sisaJam = sisaMaghrib/60;
+            sisaMenit = sisaMaghrib%60;
+            text_jam.setText(String.valueOf(sisaJam));
+            text_menit.setText(String.valueOf(sisaMenit));
+            sholat.setText("Maghrib");
+        }
+        else{
+            sisaJam = sisaIsya/60;
+            sisaMenit = sisaIsya%60;
+            text_jam.setText(String.valueOf(sisaJam));
+            text_menit.setText(String.valueOf(sisaMenit));
+            sholat.setText("Isya");
+        }
+    }
+
+    /**
+     * buat menghitung sisa waktu berdasarkan waktu saat ini
+     * @param waktuSolat
+     * @return int sisaWaktuTotal
+     */
+    public int SisaWaktuSolat(String waktuSolat){
+        final String TAG = "AsyncTaskParseJson.java";
+        int jamSekarang, menitSekarang;
+        //buat nyari tahu sekarang jam berapa dan menit ke berapa
+        Calendar cal = Calendar.getInstance();
+        jamSekarang = cal.get(Calendar.HOUR_OF_DAY);
+        menitSekarang = cal.get(Calendar.MINUTE);
+        //buat menentukan sisa waktu imsyak
+        int jamImsyak = Integer.parseInt(substring(waktuSolat, 0, 2));
+        int menitImsyak = Integer.parseInt(substring(waktuSolat, 3, 5));
+        int sisaJamImsyak, sisaMenitImsyak, sisaTotalImsyak;
+        Log.e(TAG, String.valueOf(jamImsyak));
+        Log.e(TAG, String.valueOf(menitImsyak));
+        sisaJamImsyak = jamImsyak - jamSekarang;
+        if (sisaJamImsyak<0){
+            sisaJamImsyak = 0;
+            sisaMenitImsyak = 0;
+            sisaTotalImsyak = 0;
+            return sisaTotalImsyak;
+        }
+        sisaMenitImsyak = menitImsyak - menitSekarang;
+        if (menitSekarang>menitImsyak){
+            sisaMenitImsyak = menitImsyak+60-menitSekarang;
+            sisaJamImsyak = sisaJamImsyak - 1;
+            if (sisaJamImsyak<0){
+                sisaJamImsyak = 0;
+                sisaMenitImsyak = 0;
+                sisaTotalImsyak = 0;
+            }
+        }
+        sisaTotalImsyak = (sisaJamImsyak*60)+sisaMenitImsyak;
+        Log.e(TAG, String.valueOf(sisaTotalImsyak));
+        return sisaTotalImsyak;
     }
 
     private void setupToolbar() {
